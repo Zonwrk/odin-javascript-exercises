69529873bc9269ce3a0bece33e968e11ae7971c1
zonwrk <[zonwrk@gmail.com]>
2024-08-12 11:09:19 +0200

    Did second exercise repeat String with automated testing.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let sentence = '';
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        sentence += word;
+    }
+    return sentence;
 };
 
 // Do not edit below this line
