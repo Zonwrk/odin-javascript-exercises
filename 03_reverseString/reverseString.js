50aa6d68cb18a33a14e19e7403ffc69827dd7024
zonwrk <[zonwrk@gmail.com]>
2024-08-18 22:48:07 +0200

    Finished exercise 04 where I wrote a function that removes any user inputted parameters from an array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(textToReverse) {
+    let textArray = textToReverse.split("");
+    console.log(textArray);
+    let textFinal = '';
+    for (let i = textArray.length - 1; i >= 0; i--) {
+        textFinal += textArray[i];
+    }
+    console.log(textFinal);
+    return textFinal;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+/*
+Write a function that removes any arguments from '...removeThese' rest parameter, from the inputArray.
+*/
+const removeFromArray = function(inputArray, ...removeThese) {
+    let array = inputArray.filter(item => !removeThese.includes(item));
+
+    return array;
 };
 
 // Do not edit below this line
