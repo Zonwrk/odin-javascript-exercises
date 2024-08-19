1da51d731f6b31ab9b3f1f93fe2040c5ba530b37
zonwrk <[zonwrk@gmail.com]>
2024-08-19 19:38:37 +0200

    Did exercise 05 where I summed all the numbers in between 2 variables

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
 
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0 || !Number.isInteger(number1) || !Number.isInteger(number2) 
+        || typeof number1 !== "number" || typeof number2 !== "number" ) {
+        return "ERROR";
+    }
+
+    let BiggerNumber = Math.max(number1, number2);
+    let SmallerNumber = Math.min(number1, number2);
+
+    let NumbersAdded = 0;
+    for (let i = SmallerNumber; i <= BiggerNumber; i++) {
+        NumbersAdded += i;       
+    }
+
+    return NumbersAdded;
 };
 
 // Do not edit below this line
