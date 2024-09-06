e299c16a536c64eaa56ba06824e474b93b465bc0
zonwrk <[zonwrk@gmail.com]>
2024-09-06 15:02:08 +0200

    Did the 08_calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiply, current) => multiply * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+
+  let sumFactorial = 1;
+  for (let i = 1; i <= a; i++) {
+    sumFactorial *= i; 
+  }
+  return sumFactorial;
 };
 
 // Do not edit below this line
