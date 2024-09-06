135c0a1844e1ad88b49d68752e20dd4de6fafdb3
zonwrk <[zonwrk@gmail.com]>
2024-09-06 15:21:12 +0200

    Did 09_palindromes exercise for Odin.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (stringInput) {
+    let stringLettersAndNumbers = stringInput.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let stringReverse = stringLettersAndNumbers.split('').reverse().join('');
+    return stringLettersAndNumbers === stringReverse;
 };
 
 // Do not edit below this line
